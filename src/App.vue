25599314ab40f52ac0d563eb3ff0f8d2987f2056
Mostafa Hashhash <mostafa.hashash@robustastudio.com>
2021-05-23 14:16:59 +0200

    fix(login ui): fixing the layout of the login page heigth

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script>
-import Nav from "./Category";
+import Nav from "@/Category";
 
 export default {
   name: "App",
--- src/components/login.vue
+++ src/components/login.vue
@@ -1,45 +1,55 @@
 <template>
-  <div class="r-green w-2/5 bg-green-800 inline-block">
-  
-  <img src="../assets/logo.svg" class="my-10 mx-auto" alt="Seoudi Logo">
-  <h3 class="text-3xl text-white font-bold text-center my-10">نظام عرض المطبخ</h3>
-  
-  </div>
+  <div class="relative flex min-h-screen">
+    <div class="r-green w-2/5 bg-green-800 inline-block min-h-screen pt-10">
+      <img src="../assets/logo.svg" class="my-10 mx-auto pt-10" alt="Seoudi Logo" />
+      <h3 class="text-3xl text-white font-bold text-center my-10">
+        نظام عرض المطبخ
+      </h3>
+    </div>
 
-  <div class="l-white inline-block border-2 w-3/5">
-    <div class="login-form w-1/2 rtl mx-auto">
-      <h2 class="text-3xl m-2">تسجيل الدخول</h2>
-      <form>
-        <div class="my-6">
-          <label class="p-2" for="branch">إختر الفرع</label>
-          <select
-            v-model="selectedBranch"
-            class="branch-menu py-4 px-2 w-full text-black-primary outline-none"
-            name="branch"
-          >
-            <option value="" selected disabled hidden class="">... إختر</option>
-            <option v-for="branch in b" :key="branch.id" :value="branch.value">
-              {{ branch.name }}
-            </option>
-          </select>
-        </div>
+    <div class="l-white inline-block w-3/5 pt-10 mt-10">
+      <div class="login-form w-1/2 rtl mx-auto mt-10">
+        <h2 class="text-3xl m-2">تسجيل الدخول</h2>
+        <form>
+          <div class="my-6">
+            <label class="p-2" for="branch">إختر الفرع</label>
+            <select
+              v-model="selectedBranch"
+              class="branch-menu py-4 px-2 w-full text-black-primary outline-none"
+              name="branch"
+            >
+              <option value="" selected disabled hidden class=""
+                >... إختر</option
+              >
+              <option
+                v-for="branch in b"
+                :key="branch.id"
+                :value="branch.value"
+              >
+                {{ branch.name }}
+              </option>
+            </select>
+          </div>
 
-        <div class="my-6">
-          <label for="password" class="p-2">كلمة السر</label>
-          <label for="password" class="float-left text-green-800">نسيت كلمة السر؟</label>
+          <div class="my-6">
+            <label for="password" class="p-2">كلمة السر</label>
+            <label for="password" class="float-left text-green-800"
+              >نسيت كلمة السر؟</label
+            >
+            <input
+              class="password py-3 px-5 w-full rounded bg-gray-200 outline-none"
+              type="password"
+              name="password"
+              v-model="password"
+            />
+          </div>
           <input
-            class="password py-3 px-5 w-full rounded bg-gray-200 outline-none"
-            type="password"
-            name="password"
-            v-model="password"
+            class="py-3 px-8 my-2 bg-green-800 rounded-md text-white outline-none"
+            type="submit"
+            value="تسجيل دخول"
           />
-        </div>
-        <input
-          class="py-3 px-8 my-2 bg-green-800 rounded-md text-white outline-none"
-          type="submit"
-          value="تسجيل دخول"
-        />
-      </form>
+        </form>
+      </div>
     </div>
   </div>
 </template>
@@ -60,7 +70,6 @@ export default {
 </script>
 
 <style>
-
 .r-green {
   height: 100% !important;
 }
