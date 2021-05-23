aa26cb7e9baf97b51dd8439726f0879cd2f80094
Mostafa Hashhash <mostafa.hashash@robustastudio.com>
2021-05-23 13:56:14 +0200

    feat(ui): adding outline on the selected order in the ongoing orders page

--- src/components/OngoingOrders.vue
+++ src/components/OngoingOrders.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="w-2/5 float-rigth inline-block">
     <div
-      v-for="order in orders"
+      v-for="(order, index) in orders"
       :key="order.number"
-      class="w-60 m-4 rounded-lg p-2 bg-red-500 text-white shadow-lg "
-      @click="notifySelection(order)"
+      class=" w-60 m-4 rounded-lg p-2 bg-red-500 text-white shadow-lg "
+      :id="'id' + index"
+      @click="notifySelection(order, index)"
     >
       <p class="font-bold">
         <span class="float-rigth">{{ order.name }}</span>
@@ -20,14 +21,35 @@
 
 <script>
 export default {
+  data: () => {
+    return {
+      previousId: "",
+      currentId: "",
+    };
+  },
+
   props: ["orders"],
 
   methods: {
-    notifySelection(order) {
+    notifySelection(order, index) {
+
+      this.previousId = this.currentId;
+
+      if(this.previousId)
+        document.querySelector(this.previousId).classList.remove("order-ticket");
+
+      this.currentId = "#id" + index;
+      document.querySelector(this.currentId).classList.add("order-ticket");
+
       this.$emit("orderSelection", { order: order });
     },
   },
 };
 </script>
 
-<style></style>
+<style>
+.order-ticket {
+  outline: 5px solid green;
+  outline-offset: 5px;
+}
+</style>
