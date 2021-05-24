dc1c7e4492785c88625f25b908416ff2aacf7124
Mostafa Hashhash <mostafa.hashash@robustastudio.com>
2021-05-24 11:54:59 +0200

    fix(ui): finishing the border around the selected order

--- src/components/OngoingOrders.vue
+++ src/components/OngoingOrders.vue
@@ -1,23 +1,32 @@
 <template>
-  <div class="w-2/5 float-rigth inline-block h-80 border">
+  <div class="w-3/5 float-rigth inline-block h-80 border mt-2">
     <div
       v-for="(order, index) in orders"
       :key="order.number"
-      class="w-60 m-4 rounded-lg p-3 bg-red-500 text-white shadow-lg "
+      class="w-80 mx-3 order-wrapper"
       :id="'id' + index"
       @click="notifySelection(order, index)"
     >
-      <p class="font-bold mb-2">
-        <span class="float-rigth">{{ order.name }}</span>
-        <span class="float-left">#{{ order.number }} </span>
-      </p>
-      <p>
-        <span class="float-rigth">
-          <img src="@/assets/time.svg" class="inline" alt="" width="15" />
-          {{ order.date }}</span
-        >
-        <span class="float-left">{{ order.items.length }} منتجات</span>
-      </p>
+      <div
+        class="order-red m-2 text-white p-4 rounded-lg"
+        :class="{
+          'order-red': order.status == 'new',
+          'order-orange': order.status == 'preparing',
+          'order-green': order.status == 'ready',
+        }"
+      >
+        <p class="font-bold mb-2">
+          <span class="float-rigth">{{ order.name }}</span>
+          <span class="float-left">#{{ order.number }} </span>
+        </p>
+        <p>
+          <span class="float-rigth">
+            <img src="@/assets/time.svg" class="inline" alt="" width="15" />
+            {{ order.date }}</span
+          >
+          <span class="float-left">{{ order.items.length }} منتجات</span>
+        </p>
+      </div>
     </div>
   </div>
 </template>
@@ -40,10 +49,12 @@ export default {
       if (this.previousId)
         document
           .querySelector(this.previousId)
-          .classList.remove("order-ticket");
+          .classList.remove("selected-order-wrapper");
 
       this.currentId = "#id" + index;
-      document.querySelector(this.currentId).classList.add("order-ticket");
+      document
+        .querySelector(this.currentId)
+        .classList.add("selected-order-wrapper");
 
       this.$emit("orderSelection", { order: order });
     },
@@ -52,8 +63,24 @@ export default {
 </script>
 
 <style>
-.order-ticket {
-  outline: 5px solid green;
-  outline-offset: 5px;
+.order-wrapper {
+  border: 5px solid #e5e5e5;
+}
+
+.selected-order-wrapper {
+  border: 5px solid #84c559;
+  border-radius: 10px;
+}
+
+.order-red {
+  background-color: #f24e40;
+}
+
+.order-green {
+  background-color: #84c559;
+}
+
+.order-orange {
+  background-color: #f5a623;
 }
 </style>
