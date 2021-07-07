cc0ccd6496388a847c981e10db119ce5aab33407
Mostafa Hashhash <mostafa.hashash@robustastudio.com>
2021-07-07 02:18:48 +0200

    fix(ui): fix the spacing in the new order modal

--- src/components/StartOrderModal.vue
+++ src/components/StartOrderModal.vue
@@ -1,38 +1,36 @@
 <template>
   <div class="flex justify-center items-center z-50 absolute w-full h-full">
-    <div class="bg-white p-8 mx-auto">
+    <div class="bg-white card p-4 mx-auto ">
       <img
         src="@/assets/StartOrderModalClosingIcon.svg"
-        class="cursor-pointer"
+        class="cursor-pointer absolute"
         @click="emitCloseModalEvent()"
         alt=""
       />
-      <img src="@/assets/card.svg" alt="ID Card Icon" class="mx-auto" />
-      <p class="my-4 text-center">قم بمسح الكارت الخاص بك</p>
 
-      <div class="text-center">
-        <img class="inline w-2/5" src="@/assets/bar.svg" alt="" />
-        <span class="m-4 bolck">أو</span>
-        <img class="inline w-2/5" src="@/assets/bar.svg" alt="" />
-      </div>
+      <div class="py-6 px-10 mx-10">
+        <img src="@/assets/card.svg" alt="ID Card Icon" class="mx-auto" />
 
-      <p class="my-4 text-center">أدخل الكود بدلا من ذلك</p>
+        <p class="my-4 text-center">قم بمسح الكارت الخاص بك</p>
+        <span class="m-4 bolck or">أو</span>
+        <p class="my-4 text-center">أدخل الكود بدلا من ذلك</p>
 
-      <div class="my-4 ">
-        <div class="inline-block ml-5">
-          <label class="mb-2 block" for="code">الكود</label>
-          <input
-            type="text"
-            v-model="code"
-            class="px-4 py-2 block border rounded-lg outline-none shadow-lg w-full"
-          />
+        <div class="my-4 ">
+          <div class="inline-block ml-5">
+            <label class="mb-2 block" for="code">الكود</label>
+            <input
+              type="text"
+              v-model="code"
+              class="px-4 py-2 block border rounded-lg outline-none shadow-lg w-full"
+            />
+          </div>
+
+          <button
+            class="start-order-btn bg-green-750 text-white px-6 py-2 rounded-lg shadow-xl focus:outline-none"
+          >
+            ابدأ الطلب
+          </button>
         </div>
-
-        <button
-          class="start-order-btn bg-green-750 text-white px-6 py-2 rounded-lg shadow-xl focus:outline-none"
-        >
-          ابدأ الطلب
-        </button>
       </div>
     </div>
   </div>
@@ -59,4 +57,20 @@ export default {
   box-shadow: none !important;
   background-color: #005931 !important;
 }
+
+.or {
+  display: flex;
+}
+.or::after,
+.or::before {
+  content: " ";
+  width: 100%;
+  margin: 10px;
+  padding: 5px;
+  background: url("../assets/bar.svg") center no-repeat;
+}
+
+/* .card {
+  padding: 40px 100px;
+} */
 </style>
